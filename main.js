81cba31df98fe33ee45f75ad4a60793e302a773c
AxelSchasemback <deluxe_shot.gamer@hotmail.com>
2023-10-18 16:49:54 -0300

    Desafio 1

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 class Product {
+    id
     title
     description
     price
     thumsbnail
     code
     stock
-    constructor(id, { title, description, price, thumsbnail, code, stock }) {
+    constructor( id, title, description, price, thumsbnail, code, stock ) {
         this.id = id
         this.title = title
         this.description = description
@@ -16,31 +17,82 @@ class Product {
     }
 }
 
-
 class ProductManager {
-    static #ultimoId = 0
-    #getProduct = [];
 
-static #generarNuevoID() {
+    dataProduct
+
+    static #ultimoId = 0
+
+    constructor() {
+        this.dataProduct = [];
+    }
+
+static generarNuevoID() {
     return ++ProductManager.#ultimoId;
 }
 
-    getProduct() {
-        return this.#getProduct;
-    };
+addProduct( { title, description, price, thumbnail, code, stock } ) {
 
-    addProduct(id, datos) {
-        const ap = new product(datos)
-        this.#getProduct.push(ap)
-    };
+    const codRepeat = this.dataProduct.find((codigo) => codigo.code === code)
+
+    if (!codRepeat) {
+        const id = ProductManager.generarNuevoID();       
+        const producto = new Product( id, title, description, price, thumbnail, code, stock )
+        console.log(producto)
+        this.dataProduct.push( producto )
+        return producto;
+    } else {
+
+        return console.log(`El codigo está repetido, no se puede agregar este producto`)
+
+    }
+};
+
+getProduct() {
+    return this.dataProduct;
+};
+
+getProductById (id) {
+    const searxhId = this.dataProduct.find((search) => search.id === id)
+    if (!searxhId) {
+        console.error(`la ID "${id}" solicitada no Existe`)
+    }
+    return searxhId;
 }
 
+delProduct(id) {
+    const deleteProduct = this.dataProduct.filter((borrar) => borrar.id !== id);
+    return this.dataProduct = deleteProduct;
+}}
+
 const pm = new ProductManager()
-pm.addProduct({
-    title: 'producto prueba',
-    description: 'Este es un producto prueba',
-    price: 200,
-    thumbnail: 'Sin imagen',
-    code: 'abc123',
-    stock: 25
+const p1 = pm.addProduct({
+    title: 'producto prueba1',
+    description: 'Este es un producto prueba1',
+    price: 100,
+    thumbnail: 'Sin imagen1',
+    code: 'abc1231',
+    stock: 10
 })
+
+const p2 = pm.addProduct({
+    title: 'producto prueba2',
+    description: 'Este es un producto prueba2',
+    price: 200,
+    thumbnail: 'Sin imagen2',
+    code: 'abc1232',
+    stock: 20
+})
+
+const p3 = pm.addProduct({
+    title: 'producto prueba3',
+    description: 'Este es un producto prueba3',
+    price: 300,
+    thumbnail: 'Sin imagen3',
+    code: 'abc1233',
+    stock: 30
+})
+
+console.log(pm.getProductById(2))
+console.log(pm.delProduct(2))
+console.log(pm.getProduct())
